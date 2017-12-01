954294cdc246a8f56081898a91a1636e5e8c9bc7
Karin Golan <Karingolan25@gmail.com>
2017-12-01 17:02:43 +0100

    Signed-off-by: Karin Golan <Karingolan25@gmail.com>

--- berks-cookbooks/fronend/recipes/default.rb
+++ berks-cookbooks/fronend/recipes/default.rb
@@ -5,6 +5,13 @@
 # Copyright (C) 2017 
 #
 
+ s3_file "/home/ec2-user/FronEnd.jar" do
+        remote_path "FronEnd.jar"
+        bucket "frontendbucket"
+        s3_url "https://s3-eu-west-1.amazonaws.com/frontendbucket"
+        action :create
+    end
+   
 execute "download file from  s3" do
 command "aws s3 cp s3://frontendbucket/FronEnd.jar  /home/ec2-user/FronEnd.jar"
  action "run"

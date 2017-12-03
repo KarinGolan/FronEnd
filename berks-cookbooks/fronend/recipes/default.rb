6ece021440af465e7dfff0e8917cef9f49b0bc51
Karin Golan <Karingolan25@gmail.com>
2017-12-03 11:23:35 +0100

    Signed-off-by: Karin Golan <Karingolan25@gmail.com>

--- berks-cookbooks/fronend/recipes/default.rb
+++ berks-cookbooks/fronend/recipes/default.rb
@@ -10,4 +10,8 @@ command "aws s3 cp s3://frontendbucket/FronEnd.jar   /home/ec2-user/FronEnd.jar"
  action "run"
 end
 
-
+execute "running /FronEnd" do
+  command "java -jar /home/ec2-user/FronEnd.jar"
+  cwd "/home/ec2-user"
+  action "run"
+end

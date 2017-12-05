1b85053bb09ab8bf5ccb5972fcd41353e0fde0a3
Karin Golan <Karingolan25@gmail.com>
2017-12-05 15:39:32 +0100

    Signed-off-by: Karin Golan <Karingolan25@gmail.com>

--- src/mypackage/FoodOrders.java
+++ src/mypackage/FoodOrders.java
@@ -60,7 +60,7 @@ public class FoodOrders {
 								
 			System.out.println("Hi, " + customer.mama() + " your order will arrive to " + customer.dada() + " whitin an hour, enjoy your meal");
 
-		    Socket socket = new Socket("my.backend.com",8080);
+		    Socket socket = new Socket("my.backend.com",8070);
 	        in = new BufferedReader(
 	                new InputStreamReader(socket.getInputStream()));
 	        out = new PrintWriter(socket.getOutputStream(), true);
